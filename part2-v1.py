e33989621d7c4145318151489c7b2496ce183594
VikramVenkatapathi <vikramvenkat2408@gmail.com>
2021-05-21 22:50:37 +0530

    Completed part-2

--- part2-v1.py
+++ part2-v1.py
@@ -2,6 +2,7 @@ import csv
 import sys
 import pandas as pd
 
+#to get the name from col_list based on index 'i'
 def getColName(i, col_list):
     col = col_list[i]
     return col
@@ -10,16 +11,22 @@ def getColName(i, col_list):
 def list_Toppers(df, col_list):
     for i in range(1, 7):
         col = getColName(i, col_list)
+
+		#find the highest mark in a single subject
         max_marks.append(max(df[col]))
 
+		#Retreiving only the 'Name' and column to find the topper(s)
         name = pd.read_csv(file_name, usecols=["Name", col], index_col=False)
 
+		#Getting the name and marks of the student 
+		# if he/she scores the highest mark in that subject, and converting them to a list
         temp = name[name[col] == max_marks[i-1]].values.tolist()
         if(len(temp) == 1):
             print("\nTopper in ", col, " is ", temp[0][0])
         if(len(temp) == 2):
             print("\nTopper in ", col, " is ", temp[0][0], ",", temp[1][0])
 
+#to get the second element from the list
 def takeSecond(elem):
     return elem[1]
 
@@ -34,7 +41,7 @@ def list_Ranks(df, col_list, file_name):
 			temp.append(row[0])
 			temp.append(total)
 			total_marks.append(temp)
-	#sorting the list based on the total in reverse order
+	#sorting the list based on the student's total in reverse order
 	total_marks.sort(key=takeSecond,reverse=True)
 	print("\nBest students in the class are ",total_marks[0][0],",",total_marks[1][0],",",total_marks[2][0],"\n")
 
